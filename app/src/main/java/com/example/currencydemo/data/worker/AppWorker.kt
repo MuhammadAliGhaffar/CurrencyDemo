dd8fa46d5a9bd7cc529377573f6a8d68f5673d09
MuhammadAliGhaffar <ali.ghaffar@purevpn.com>
2023-09-13 12:15:02 +0500

    save in pref

--- app/src/main/java/com/example/currencydemo/data/worker/AppWorker.kt
+++ app/src/main/java/com/example/currencydemo/data/worker/AppWorker.kt
@@ -9,6 +9,7 @@ import com.example.currencydemo.R
 import com.example.currencydemo.data.repository.Repository
 import com.example.currencydemo.data.utilities.API_KEY
 import com.example.currencydemo.data.utilities.NetworkResult
+import com.example.currencydemo.data.utilities.PersistenceStorage
 import com.example.currencydemo.data.utilities.Utils.toast
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -21,17 +22,16 @@ import kotlinx.coroutines.withContext
 class AppWorker @AssistedInject constructor(
     @Assisted appContext: Context,
     @Assisted workerParams: WorkerParameters,
-    private val repository: Repository
+    private val repository: Repository,
+    private val persistenceStorage: PersistenceStorage
 ) : CoroutineWorker(appContext, workerParams) {
     override suspend fun doWork(): Result {
         withContext(Dispatchers.IO) {
             repository.getCurrencies(API_KEY).onEach {
                 when (it) {
                     is NetworkResult.Success -> {
-
-//                        repository.allDatabaseUsers().userDao().insertUser(it)
-                        toast(applicationContext,"${it.data}")
-                        Log.d("_debug", "Worker Called - Updated list added in Database ${it.data}")
+                        persistenceStorage.setCurrencies(it.data)
+                        Log.d("_debug", "Worker Called - Updated data added in pref ${it.data}")
                     }
                     else -> {}
                 }
